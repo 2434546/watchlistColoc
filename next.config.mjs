dc3b058698fb2a34fa7b788372cbce8603a6a742
2434546 <2434546@etu.cchic.ca>
2025-02-22 23:23:47 +0100

    Fix dependency conflictshhqsdq

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        domains: ["image.tmdb.org"], // Ajoutez le domaine ici
+    },
 };
 
 export default nextConfig;
